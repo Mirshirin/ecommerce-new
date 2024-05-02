5134f18299191c4d3e7f69ac471c5a57a6c32a5c
Mirshirin <mirshirin8353@gmail.com>
2024-05-02 22:58:38 +0330

    add index file for built-in route

--- resources/views/home/layouts/header.blade.php
+++ resources/views/home/layouts/header.blade.php
@@ -1,6 +1,6 @@
 
 <header class="header_section">
-    <div class="container">
+    <div class="container">کن
        <nav class="navbar navbar-expand-lg custom_nav-container ">
           <a class="navbar-brand" href="index.html"><img width="250" src="images/logo.png" alt="#" /></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
